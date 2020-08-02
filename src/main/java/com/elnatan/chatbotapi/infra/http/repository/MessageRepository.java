ce560680b2163daad284f2b3a3a512360238b937
Natan Oliveira <natan.oliveira0010@gmail.com>
2020-08-02 16:33:36 -0300

    removed annotation @Param

--- src/main/java/com/elnatan/chatbotapi/infra/http/repository/MessageRepository.java
+++ src/main/java/com/elnatan/chatbotapi/infra/http/repository/MessageRepository.java
@@ -2,7 +2,6 @@ package com.elnatan.chatbotapi.infra.http.repository;
 
 import com.elnatan.chatbotapi.entity.MessageEntity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -11,5 +10,5 @@ import java.util.List;
 public interface MessageRepository extends CrudRepository<MessageEntity, String> {
     @Override
     <S extends MessageEntity> S save(S entity);
-    List<MessageEntity> findByAuthor(@Param("name") String name);
+    List<MessageEntity> findByAuthor(String author);
 }
